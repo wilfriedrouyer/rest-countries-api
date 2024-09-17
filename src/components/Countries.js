58747ce46d3347fe9f135fe16c565ab8bf1a6e40
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2024-09-17 18:23:49 +0200

    Feat: Ajout des filtres et de la barre de recherche

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import { GetCountries } from '../api/CountriesApi';
 import CountryCard from './CountryCard';
@@ -8,6 +10,12 @@ import '../styles/Countries.css';
 export default function Countries() {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
+
+  const [nameFilter, setNameFilter] = useState('');
+
+  const [regionFilter, setRegionFilter] = useState('default');
+
+  const regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
 
   useEffect(() => {
     const loadCountries = async () => {
@@ -22,23 +30,63 @@ export default function Countries() {
     loadCountries();
   }, []);
 
+  const filterCountry = (e) => {
+    setNameFilter(e.target.value.toLowerCase());
+  };
+
+  const filterRegion = (e) => {
+    let region = e.target.value;
+    if(region === "America")
+      region = "Americas";
+    setRegionFilter(region);
+  };
+
+  console.log(countries);
+
   if (error) return <div>{error}</div>;
 
   return (
-    <ul className="country-list">
-      {countries
-        .sort((country, countryToCompare) =>
-          country.name.common > countryToCompare.name.common
-            ? 1
-            : country.name.common < countryToCompare.name.common
-            ? -1
-            : 0
-        )
-        .map((country) => (
-          <li key={country.cca3}>
+    <div className="container">
+      <div className="controls">
+        <div className="search-country">
+          <FontAwesomeIcon className="icon-search-bar" icon={faSearch} />
+          <input
+            className="control-country search-bar"
+            placeholder="Search for a country..."
+            onChange={filterCountry}
+          />
+        </div>
+        <select className="control-country select-region" onChange={filterRegion}>
+          <option value="default">Filter by Region</option>
+          {regions.map((region) => (
+            <option value={region} key={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="country-list">
+        {countries
+          .sort((country, countryToCompare) =>
+            country.name.common > countryToCompare.name.common
+              ? 1
+              : country.name.common < countryToCompare.name.common
+              ? -1
+              : 0
+          )
+          .filter((country) =>
+            country.name.common.toLowerCase().startsWith(nameFilter)
+          )
+          .filter((country) => {
+            if (regionFilter !== 'default')
+              return country.region === regionFilter;
+
+            return true;
+          })
+          .map((country) => (
             <CountryCard country={country} />
-          </li>
-        ))}
-    </ul>
+          ))}
+      </div>
+    </div>
   );
 }
