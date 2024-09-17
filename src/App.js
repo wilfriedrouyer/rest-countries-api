2db3eab347d6cb58a092e8286d493b8f7c0fea8f
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2024-09-17 11:46:34 +0200

    Feat: Using Countries Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './styles/App.css';
+import Countries from './components/Countries';
 
 function App() {
   return (
-    <div>
-      <></>
+    <div className='app-body'>
+      <Countries/>
     </div>
   );
 }
